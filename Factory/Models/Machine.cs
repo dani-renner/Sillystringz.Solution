54b93fb29f5721862ee1186d03871b470a621853
Dani Renner <danijrenner@gmail.com>
2021-03-19 11:41:49 -0700

    add Machine model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public string Description {get; set;}
+    public int EngineerId { get; set; }
+    public virtual Engineer Engineer { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+  }
+}
