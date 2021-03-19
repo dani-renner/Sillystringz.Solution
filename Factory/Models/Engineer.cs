2f0fb74215a26429376d9b2e2a9306c33616f5bf
Dani Renner <danijrenner@gmail.com>
2021-03-19 10:54:04 -0700

    add context and Engineer controller+model

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set;}
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+  }
+}
