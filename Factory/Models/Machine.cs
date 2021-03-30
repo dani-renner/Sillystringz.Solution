835fd89c414ec6e8c12ba7afda97ae7763bdeacf
Dani Renner <danijrenner@gmail.com>
2021-03-29 20:58:52 -0700

    remove Migrations, remove EngineerID from Machine

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,7 +6,6 @@ namespace Factory.Models
     public int MachineId { get; set; }
     public string Name { get; set; }
     public string Description {get; set;}
-    public virtual Engineer Engineer { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; }
     public Machine()
     {
